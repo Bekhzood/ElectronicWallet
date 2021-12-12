a6b3c6f6466f1cda5ccc391d85dfb28d5dec5bdd
Bekhzood <bekhzod.tokhirjonov@billz.uz>
2021-12-13 02:28:18 +0500

    finishes ChechAccount,GetBalance and UpdateBalance funcs

--- api/handlers/v1/account.go
+++ api/handlers/v1/account.go
@@ -1,10 +1,57 @@
 package v1
 
 import (
+	"github.com/Bekhzood/ElectronicWallet/models"
 	"github.com/Bekhzood/ElectronicWallet/storage"
 	"github.com/gin-gonic/gin"
 )
 
-func (h *Handler) GetAccount(c *gin.Context) {
-	storage.NewAccountRepo(h.storagePostgres).GetList()
+func (h *Handler) CheckAccount(c *gin.Context) {
+	id := c.Param("id")
+
+	res, err := storage.NewAccountRepo(h.storagePostgres).CheckAccount(id)
+	if err != nil {
+		h.handleErrorResponse(c, 400, "bad request", err.Error())
+		return
+	}
+
+	h.handleSuccessResponse(c, 200, "ok", res)
+	return
+}
+
+func (h *Handler) GetTransactionsHistory(c *gin.Context) {
+	storage.NewAccountRepo(h.storagePostgres).GetHistory()
+}
+
+func (h *Handler) GetBalance(c *gin.Context) {
+	id := c.Param("id")
+
+	res, err := storage.NewAccountRepo(h.storagePostgres).GetBalance(id)
+
+	if err != nil {
+		h.handleErrorResponse(c, 400, "bad request", err.Error())
+		return
+	}
+
+	h.handleSuccessResponse(c, 200, "ok", res)
+	return
+}
+
+func (h *Handler) UpdateBalance(c *gin.Context) {
+	number := c.Param("id")
+	wallet := models.Wallet{}
+
+	if err := c.ShouldBindJSON(&wallet); err != nil {
+		h.handleErrorResponse(c, 400, "bad request", err.Error())
+	}
+
+	res, err := storage.NewAccountRepo(h.storagePostgres).UpdateBalance(number, wallet.Balance)
+
+	if err != nil {
+		h.handleErrorResponse(c, 400, "bad request", err.Error())
+		return
+	}
+
+	h.handleSuccessResponse(c, 200, "ok", res)
+	return
 }
--- storage/account.go
+++ storage/account.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
@@ -14,10 +16,42 @@ func NewAccountRepo(db *sqlx.DB) *accountRepo {
 	return &accountRepo{db: db}
 }
 
-func (a *accountRepo) GetList() {
+func (a *accountRepo) GetPassword(username string) (string, error) {
+	var password string
+	query := `SELECT password
+		FROM
+			users 
+		WHERE username = $1`
+
+	rows, err := a.db.Query(query, username)
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	if rows.Next() {
+		rows.Scan(&password)
+	}
+
+	return password, nil
+}
+
+func (a *accountRepo) CheckAccount(walletNumber string) (bool, error) {
+	var id string
+	query := `SELECT id FROM wallets WHERE number = $1`
+
+	err := a.db.QueryRow(query, walletNumber).Scan(&id)
+
+	if err != nil || err == sql.ErrNoRows {
+		return false, errors.New("No account found")
+	}
+
+	return true, nil
+}
+
+func (a *accountRepo) GetHistory() {
 	query := `SELECT id
 	FROM
-		accounts`
+		geozones`
 
 	rows, _ := a.db.Query(query)
 	var id string
@@ -26,3 +60,71 @@ func (a *accountRepo) GetList() {
 		fmt.Println(id)
 	}
 }
+
+func (a *accountRepo) GetBalance(walletNumber string) (int, error) {
+	var balance int
+	query := `SELECT balance FROM wallets WHERE number = $1`
+
+	err := a.db.QueryRow(query, walletNumber).Scan(&balance)
+	if err != nil {
+		return 0, err
+	}
+
+	return balance, nil
+}
+
+func (a *accountRepo) UpdateBalance(walletNumber string, additionalMoney int) (rowsAffected int64, err error) {
+	var (
+		balance    int
+		walletType string
+	)
+	query := `SELECT balance,type FROM wallets WHERE number = $1`
+	checkErr := a.db.QueryRow(query, walletNumber).Scan(&balance, &walletType)
+	if checkErr != nil {
+		return 0, err
+	}
+
+	if walletType == "identified" && balance+additionalMoney > 100000 {
+		return 0, errors.New("Balance can't be more than 100 000 somoni for identified wallets")
+	}
+
+	if walletType == "unidentified" && balance+additionalMoney > 10000 {
+		return 0, errors.New("Balance can't be more than 10 000 somoni for unidentified wallets")
+	}
+
+	updateQuery := `UPDATE wallets SET balance = balance + $1 WHERE number = $2`
+
+	result, err := a.db.Exec(updateQuery, additionalMoney, walletNumber)
+	if err != nil {
+		return 0, err
+	}
+
+	rowsAffected, err = result.RowsAffected()
+	if err != nil {
+		return 0, err
+	}
+
+	return rowsAffected, nil
+}
+
+// func (a *accountRepo) CheckAccount(walletNumber string) (bool, error) {
+// 	var count int
+// 	query := `SELECT COUNT(1) FROM wallets WHERE number = $1`
+
+// 	row, err := a.db.Query(query, walletNumber)
+
+// 	defer row.Close()
+// 	if row.Next() {
+// 		err = row.Scan(
+// 			&count,
+// 		)
+// 		if err != nil {
+// 			return false, err
+// 		}
+// 	}
+// 	if count == 0 {
+// 		return false, errors.New("No account found")
+// 	}
+// 	fmt.Println(count)
+// 	return true, nil
+// }
